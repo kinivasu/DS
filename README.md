21f0d6b8e7da673d7f0a376f79946117e96b0326
kinivasu <vasudevkinit2001@gmail.com>
2021-08-21 19:33:51 +0530

    initial comm

---
+++ README.md
@@ -0,0 +1,3 @@
+<!--https://medium.com/echohub/write-simple-and-effective-markdown-tips-8e01fdddd70 -->
+
+# Practice Repo
---
+++ mytemplate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+#define ll long long int
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+int main()
+{
+}
